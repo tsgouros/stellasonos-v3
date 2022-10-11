70fa97d0cf8b1aec58b81113e12e207056c60aa0
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2022-10-11 11:04:11 -0400

    toolbar

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View } from 'react-native';
 import ImagePage from './views/ImagePage.js';
 
-const SELECTED_IMAGE = 1
+const SELECTED_IMAGE = 11
 export default function App() {
   return (
     <View style={styles.container}>
@@ -17,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
--- views/ImagePage.js
+++ views/ImagePage.js
@@ -1,27 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View } from 'react-native';
-import images from '../images.json'
+import { StyleSheet, Image, Text, View } from 'react-native';
+import images from '../images.json';
+import { AntDesign } from '@expo/vector-icons';
 
 
 export default function ImagePage(props) {
     console.log(images.images[props.selectedImageID].src)
   return (
-    <View style={styles.container}>
-      <Image style={styles.tinyLogo} source= {{uri: images.images[props.selectedImageID].src}}></Image> 
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+                <Text style={styles.text} > {images.images[props.selectedImageID].title}: {"\n"}{images.images[props.selectedImageID].description}</Text>
+
+        <View style={styles.imageContainer}>
+          
+        <Image  style={styles.tinyLogo} source= {{uri: images.images[props.selectedImageID].src}}></Image>
+        </View>
+         
+        <StatusBar style="auto"/>
+
+        <View style = {styles.toolBar}>
+        <AntDesign name="leftcircleo" size={50} color="black" />
+        <AntDesign name="rightcircleo" size={50} color="black" />
+        <AntDesign name="upcircleo" size={50} color="black" />
+        <AntDesign  name="downcircleo" size={50}  color="black" />
+
+
+        </View>
+        </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  tinyLogo: {
-    width: 500,
-    height: 500,
+  imageContainer: {
+    width: 350,
+    height: 300,
+    backgroundColor: '#000',
   },
+  tinyLogo: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  toolBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: 350,
+    top: 50,
+  },
+  text: { 
+    bottom: 75,
+
+    width: 250,
+    alignItems: 'center',
+    textAlign: 'center', 
+
+  }
 });
