39798baf436361632f83842c7130c13c634be558
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2022-11-30 01:05:53 -0500

    update with different impact buttons

--- pages/VibrationPage.js
+++ pages/VibrationPage.js
@@ -12,6 +12,7 @@ import * as Haptics from "expo-haptics";
 import { HapticsProvider } from "react-native-custom-haptics";
 import { useHaptics } from "react-native-custom-haptics";
 import { TouchableOpacity } from "react-native-gesture-handler";
+// import ReactNativeHapticFeedback from "react-native-haptic-feedback";
 
 // const Separator = () => {
 //     return <View />;
@@ -22,43 +23,114 @@ export default function VibrationPage({ navigation }) {
 
   const PATTERN = [1 * ONE_SECOND_IN_MS, 2 * ONE_SECOND_IN_MS];
 
-  const PATTERN_DESC =
-    Platform.OS === "android"
-      ? "wait 1s, vibrate 2s, wait 3s"
-      : "wait 1s, vibrate, wait 2s, vibrate, wait 3s";
 
-  const SUCCESS_PATTERN = ["heavy", 300, "light", 300, "heavy"];
+  const SUCCESS_PATTERN = ["heavy", 300, "Light", 300, "heavy"];
+
+  // [Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
+  // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
+  // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
+  // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
+  // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
+  // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
+  // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
+  // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),]
 
   const { trigger, stop } = useHaptics();
-  React.useEffect(() => () => stop(), []);
+  React.useEffect(() => {
+    // stops the haptic pattern on cleanup
+    return () => stop();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={[styles.header, styles.paragraph]}>Cool Vibes!</Text>
+
+      {/* 100ms in between each haptic impact for 5 seconds */}
+      <Text>IMPACT HAPTIC</Text>
+      <View style={styles.impactRow}>
+        <TouchableOpacity
+          style={styles.centered}
+          title="Vibrate with trigger"
+          onPress={() => {
+            const interval = setInterval(
+              () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy),
+              100
+            );
+            setTimeout(() => clearInterval(interval), 5000);
+          }}
+        >
+          <View style={styles.square1} />
+          <Text>heavy impact </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.centered}
+          title="Vibrate with trigger"
+          onPress={() => {
+            const interval = setInterval(
+              () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.light),
+              100
+            );
+            // it will vibrate for 5 seconds
+            setTimeout(() => clearInterval(interval), 5000);
+          }}
+        >
+          <View style={styles.square2} />
+          <Text>light impact </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.centered}
+          title="Vibrate with trigger"
+          onPress={() => {
+            const interval = setInterval(
+              () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium),
+              500
+            );
+            // it will vibrate for 5 seconds
+            setTimeout(() => clearInterval(interval), 5000);
+          }}
+        >
+          <View style={styles.square3} />
+          <Text>medium impact</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      {/* VIBRATE */}
+      <View style={styles.vibrateRow}>
+        <TouchableOpacity
+          style={styles.centered}
+          title="Vibrate with trigger"
+          onPress={() => {
+            Vibration.vibrate();
+          }}
+        >
+          <View style={styles.square4} />
+          <Text>vibrate once </Text>
+        </TouchableOpacity>
+
+        <HapticsProvider>
+          <View>
+          <Button
+            style={styles.centered}
+            title="impacts with pattern"
+            onPress={() => trigger(SUCCESS_PATTERN, true)}
+          ></Button>
+          </View>
+          </HapticsProvider>
+
+
+
+      </View>
+
+      {/*       
       <TouchableOpacity 
         style = {styles.centered}
-        title="Vibrate with trigger" onPress=
-        {() => {
-          // [Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
-          // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
-          // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
-          // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
-          // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
-          // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
-          // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),
-          // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy ),]
-          const interval = setInterval(
-            () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.heavy),
-            100
-          );
-          // it will vibrate for 5 seconds
-          setTimeout(() => clearInterval(interval), 10000);
-        }}
+        title="Vibrate with pattern with trigger" onPress=
+        {() => {ReactNativeHapticFeedback.trigger("impactLight", )}}
         ><View style= {styles.square}/>
-      </TouchableOpacity>
-      <View>
-        <Button title="Vibrate once" onPress={() => Vibration.vibrate()} />
-      </View>
+      </TouchableOpacity> */}
       {/* <Separator />
         {Platform.OS == "android"
           ? [
@@ -77,7 +149,7 @@ export default function VibrationPage({ navigation }) {
               <Separator />
             ]
           : null} */}
-      <Text style={styles.paragraph}>Pattern: {PATTERN_DESC}</Text>
+      {/* <Text style={styles.paragraph}>Pattern: {PATTERN_DESC}</Text> */}
       <Button
         title="Vibrate with pattern"
         onPress={() => {
@@ -86,7 +158,6 @@ export default function VibrationPage({ navigation }) {
           setTimeout(() => clearInterval(interval), 10000);
         }}
       />
-      
 
       {/* <Separator /> */}
       <Button
@@ -96,10 +167,9 @@ export default function VibrationPage({ navigation }) {
       {/* <Separator /> */}
       <Button
         title="impact haptic"
-        onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.light)}
+        onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)}
         color="#FF0000"
       />
-     
     </SafeAreaView>
   );
 }
@@ -107,9 +177,9 @@ export default function VibrationPage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    paddingTop: 44,
-    padding: 8,
+    alignItems: "center",
+    // // paddingTop: 44,
+    // padding: 8,
   },
   header: {
     fontSize: 18,
@@ -117,23 +187,57 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   paragraph: {
-    margin: 24,
+    margin: 75,
     textAlign: "center",
   },
   separator: {
-    marginVertical: 8,
+    marginVertical: 10,
     borderBottomColor: "#737373",
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
-  square: {
+  square1: {
     height: 40,
     width: 40,
     backgroundColor: "blue",
     justifyContent: "center",
-
+  },
+  square2: {
+    height: 40,
+    width: 40,
+    backgroundColor: "green",
+    justifyContent: "center",
+  },
+  square3: {
+    height: 40,
+    width: 40,
+    backgroundColor: "red",
+    justifyContent: "center",
+  },
+  square4: {
+    height: 40,
+    width: 40,
+    backgroundColor: "purple",
+    justifyContent: "center",
   },
   centered: {
     justifyContent: "center",
     alignItems: "center",
+    alignContent: "stretch",
+
+    // if you want to fill rows left to right
+  },
+  impactRow: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignContent: "stretch",
+    flexWrap: "wrap",
+    position: "relative",
+  },
+  vibrateRow: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignContent: "stretch",
+    flexWrap: "wrap",
+    position: "relative",
   },
 });
